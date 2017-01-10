db49c305848ab46bd634cc916df3cbd8776d954a
NazariyM <nazarriko@gmail.com>
2017-01-10 18:29:39 +0200

    added map

--- build/js/app.js
+++ build/js/app.js
@@ -70,4 +70,66 @@ $(document).ready(function() {
         // ]
     });
 
+    // top scroll
+    (function () {
+        $('.js-scroll-top').click(function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, "slow");
+            return false;
+        });
+    })();
+
+    // map
+
+    (function () {
+        var myLatLng = [56.01421991876897, 37.552129938990376];
+        ymaps.ready(init);
+        var myMap;
+
+        function init() {
+            myMap = new ymaps.Map("map", {
+                center: myLatLng,
+                zoom: 15
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
+                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                    hintContent: 'Русский Терем'
+                });
+            myMap.geoObjects.add(myPlacemark);
+            myMap.behaviors.disable('scrollZoom');
+            myMap.controls.remove('searchControl');
+            myMap.controls.remove('trafficControl');
+            myMap.controls.remove('typeSelector');
+            myMap.controls.remove('fullscreenControl');
+        }
+    })();
+
+    // (function () {
+    //     var myLatLng = [56.01421991876897, 37.552129938990376],
+    //         myPlacemark = new YMaps.Placemark(new YMaps.GeoPoint(56.014036705382004, 37.55207205754845));
+    //     YMaps.ready(initMap);
+    //     var myMap;
+    //
+    //     myPlacemark.name = "Москва";
+    //     myPlacemark.description = "Столица Российской Федерации";
+    //
+    //     function initMap() {
+    //         myMap = new YMaps.Map("map", {
+    //             center: myLatLng,
+    //             zoom: 15
+    //         }, {
+    //             searchControlProvider: 'yandex#search'
+    //         });
+    //
+    //         // map.addOverlay(myPlacemark);
+    //         // myMap.behaviors.disable('scrollZoom');
+    //         // myMap.controls.remove('searchControl');
+    //         // myMap.controls.remove('trafficControl');
+    //         // myMap.controls.remove('typeSelector');
+    //         // myMap.controls.remove('fullscreenControl');
+    //     }
+    // })();
+
 });
